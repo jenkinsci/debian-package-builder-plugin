7dedd355e5ef2c2d918c3ae633650d2cb82e6b04
pupssman <pupssman@yandex-team.ru>
2014-01-17 15:03:50 +0400

    Fix indentation to use tabs instead of spaces

--- src/main/java/ru/yandex/jenkins/plugins/debuilder/DebianizingException.java
+++ src/main/java/ru/yandex/jenkins/plugins/debuilder/DebianizingException.java
@@ -2,12 +2,12 @@ package ru.yandex.jenkins.plugins.debuilder;
 
 @SuppressWarnings("serial")
 public class DebianizingException extends Exception {
-    public DebianizingException(String message) {
-        super(message);
-    }
+	public DebianizingException(String message) {
+		super(message);
+	}
 
-    public DebianizingException(String message, Throwable cause) {
-        super(message, cause);
-    }
+	public DebianizingException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
 }
