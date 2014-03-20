cb75dd88f5b7e116cde373e3f55bc50aff969df5
Caio Cezar <raizdepi@gmail.com>
2014-03-20 15:28:16 -0300

    More comments for classes

--- src/main/java/ru/yandex/jenkins/plugins/debuilder/DebUtils.java
+++ src/main/java/ru/yandex/jenkins/plugins/debuilder/DebUtils.java
@@ -12,13 +12,34 @@ import java.text.MessageFormat;
 
 import com.google.common.io.CharStreams;
 
+/**
+ * A collection of tools to help the build and publish process
+ * 
+ */
 public class DebUtils {
+    /**
+     * Provides same tools to interact with the user console and the build
+     * environment
+     * 
+     */
 	public static class Runner {
 		private final AbstractBuild<?, ?> build;
 		private final Launcher launcher;
 		private final BuildListener listener;
 		private final String prefix;
 
+        /**
+         * Create a new tool for the specific build process
+         * 
+         * @param build
+         *            The targeted build
+         * @param launcher
+         *            The launcher with the necessary envs
+         * @param listener
+         *            The build listener to get the events
+         * @param prefix
+         *            The message prefix used when messages are printed
+         */
 		public Runner(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener, String prefix) {
 			this.build = build;
 			this.launcher = launcher;
@@ -26,12 +47,37 @@ public class DebUtils {
 			this.prefix = prefix;
 		}
 
+        /**
+         * Same as {@link Runner#runCommandForResult(String)}
+         * 
+         * @param command
+         *            The command
+         * @throws InterruptedException
+         *             Same as {@link Runner#runCommandForResult(String)}
+         * @throws DebianizingException
+         *             If the {@link Runner#runCommandForResult(String)} returns
+         *             false
+         */
 		public void runCommand(String command) throws InterruptedException, DebianizingException {
 			if (!this.runCommandForResult(command)) {
 				throw new DebianizingException(MessageFormat.format("Command <{0}> failed", command));
 			}
 		}
 
+        /**
+         * Same as {@link Runner#runCommand(String)} but with using a
+         * {@link MessageFormat}
+         * 
+         * @param commandTemplate
+         *            The message template
+         * @param arguments
+         *            The template arguments
+         * @throws InterruptedException
+         *             Same as {@link Runner#runCommandForResult(String)}
+         * @throws DebianizingException
+         *             If the {@link Runner#runCommandForResult(String)} returns
+         *             false
+         */
 		public void runCommand(String commandTemplate, Object... arguments) throws InterruptedException, DebianizingException {
 			String command = MessageFormat.format(commandTemplate, arguments);
 			if (!this.runCommandForResult(command)) {
@@ -39,7 +85,19 @@ public class DebUtils {
 			}
 		}
 
-		public boolean runCommandForResult(String command) throws InterruptedException, DebianizingException {
+        /**
+         * Execute the
+         * {@link Shell#perform(AbstractBuild, Launcher, BuildListener)}
+         * 
+         * @param command
+         *            The command
+         * @return Same as
+         *         {@link Shell#perform(AbstractBuild, Launcher, BuildListener)}
+         * @throws InterruptedException
+         *             Same as
+         *             {@link Shell#perform(AbstractBuild, Launcher, BuildListener)}
+         */
+        public boolean runCommandForResult(String command) throws InterruptedException {
 			announce("running command <{0}>", command);
 			return new Shell(command).perform(build, launcher, listener);
 		}
@@ -48,10 +106,32 @@ public class DebUtils {
 			return this.runCommandForResult(MessageFormat.format(commandTemplate, arguments));
 		}
 
+        /**
+         * Same as {@link Runner#runCommandForOutput(String)} but with a
+         * {@link MessageFormat}
+         * 
+         * @param commandTemplate
+         *            The command template
+         * @param params
+         *            The template parameters
+         * @return Same as {@link Runner#runCommandForOutput(String)}
+         * @throws DebianizingException
+         *             Same as {@link Runner#runCommandForOutput(String)}
+         */
 		public String runCommandForOutput(String commandTemplate, Object... params) throws DebianizingException {
 			return runCommandForOutput(MessageFormat.format(commandTemplate, params));
 		}
 
+        /**
+         * Run the command inside a bash using
+         * {@link ProcStarter#cmdAsSingleString(String)} and return the output
+         * 
+         * @param command
+         *            The command
+         * @return The command output
+         * @throws DebianizingException
+         *             If there is some problem running the command
+         */
 		public String runCommandForOutput(String command) throws DebianizingException {
 			try {
 				String actualCommand = MessageFormat.format("bash -c ''{0}''", command);
@@ -62,18 +142,43 @@ public class DebUtils {
 			}
 		}
 
+        /**
+         * Print a new message line to user console with a prefix identification
+         * 
+         * @param message
+         *            The message
+         */
 		public void announce(String message) {
 			listener.getLogger().println(MessageFormat.format("[{0}] {1}" , prefix, message));
 		}
 
+        /**
+         * Same as {@link Runner#announce(String)} but with a
+         * {@link MessageFormat}
+         * 
+         * @param messageTemplate
+         *            The message template
+         * @param params
+         *            The parameters to the message template
+         */
 		public void announce(String messageTemplate, Object... params) {
 			this.announce(MessageFormat.format(messageTemplate, params));
 		}
 
+        /**
+         * Same as {@link Launcher#getChannel()}
+         * 
+         * @return Same as {@link Launcher#getChannel()}
+         */
 		public VirtualChannel getChannel() {
 			return launcher.getChannel();
 		}
 
+        /**
+         * Get the {@link BuildListener}
+         * 
+         * @return The {@link BuildListener}
+         */
 		public BuildListener getListener() {
 			return listener;
 		}
