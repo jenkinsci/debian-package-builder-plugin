026816666debb5ce4fb8f815faa80a80e8068cf4
pupssman <pupssman@yandex-team.ru>
2014-09-29 21:30:46 +0400

    Extract useful methods

--- src/test/java/ru/yandex/jenkins/plugins/debuilder/SmokeTest.java
+++ src/test/java/ru/yandex/jenkins/plugins/debuilder/SmokeTest.java
@@ -24,9 +24,49 @@ public class SmokeTest {
 	 * @throws Exception
 	 */
 	@Test
-	public void smoke() throws Exception {
+	public void smokeWithoutChangelog() throws Exception {
 		FreeStyleProject project = j.createFreeStyleProject();
 
+		DebianPackageBuilder builder = spy(new DebianPackageBuilder(".", "", false, false, true));
+
+		mockTestDescriptor(builder);
+		Runner runner = mockBasicRunner(builder);
+
+		project.getBuildersList().add(builder);
+		project.scheduleBuild2(0).get();
+
+		verifyInstallAndKeyImport(runner);
+		verify(runner).runCommandForOutput(contains("dpkg-parsechangelog"), contains("debian"));
+		verify(runner).runCommand(contains("pbuilder-satisfydepends"), anyVararg());
+		verify(runner, atLeast(0)).announce(anyString());
+		verify(runner, atLeast(0)).announce(anyString(), anyVararg());
+		verify(runner).runCommand(contains("debuild"));
+		verifyNoMoreInteractions(runner);
+	}
+
+	private Runner mockBasicRunner(DebianPackageBuilder builder) throws InterruptedException, DebianizingException {
+		Runner runner = mock(Runner.class);
+		doReturn(true).when(runner).runCommandForResult(any(String.class));
+		doReturn("").when(runner).runCommandForOutput(any(String.class));
+		doReturn("").when(runner).runCommandForOutput(any(String.class), anyVararg());
+
+		doReturn(runner).when(builder).makeRunner(Mockito.any(AbstractBuild.class), Mockito.any(Launcher.class), Mockito.any(BuildListener.class));
+		return runner;
+	}
+
+	public void verifyInstallAndKeyImport(Runner runner) throws InterruptedException, DebianizingException {
+		verify(runner).runCommand("sudo apt-get -y update");
+		verify(runner).runCommand("sudo apt-get -y install aptitude pbuilder");
+		verify(runner).runCommandForResult("gpg --list-key {0}", "foo@bar.com");
+		verify(runner).runCommandForResult("gpg --list-secret-key {0}", "foo@bar.com");
+		verify(runner, times(2)).runCommand(contains("gpg --import"), anyVararg());
+	}
+
+	public void mockTestDescriptor(DebianPackageBuilder builder) {
+		doReturn(getTestDescriptor()).when(builder).getDescriptor();
+	}
+
+	public DescriptorImpl getTestDescriptor() {
 		DescriptorImpl descriptorImpl = new DebianPackageBuilder.DescriptorImpl();
 
 		descriptorImpl.setAccountEmail("foo@bar.com");
@@ -35,31 +75,7 @@ public class SmokeTest {
 		descriptorImpl.setPrivateKey("ololo not a key");
 		descriptorImpl.setPassphrase("a passphrase");
 
-		DebianPackageBuilder builder = spy(new DebianPackageBuilder(".", "", false, false, true));
-		doReturn(descriptorImpl).when(builder).getDescriptor();
-
-		Runner runner = mock(Runner.class);
-		doReturn(true).when(runner).runCommandForResult(any(String.class));
-		doReturn("").when(runner).runCommandForOutput(any(String.class));
-		doReturn("").when(runner).runCommandForOutput(any(String.class), anyVararg());
-
-		doReturn(runner).when(builder).makeRunner(Mockito.any(AbstractBuild.class), Mockito.any(Launcher.class), Mockito.any(BuildListener.class));
-
-		project.getBuildersList().add(builder);
-
-		project.scheduleBuild2(0).get();
-
-		verify(runner).runCommand("sudo apt-get -y update");
-		verify(runner).runCommand("sudo apt-get -y install aptitude pbuilder");
-		verify(runner).runCommandForResult("gpg --list-key {0}", "foo@bar.com");
-		verify(runner).runCommandForResult("gpg --list-secret-key {0}", "foo@bar.com");
-		verify(runner, times(2)).runCommand(contains("gpg --import"), anyVararg());
-		verify(runner).runCommandForOutput(contains("dpkg-parsechangelog"), contains("debian"));
-		verify(runner).runCommand(contains("pbuilder-satisfydepends"), anyVararg());
-		verify(runner, atLeast(0)).announce(anyString());
-		verify(runner, atLeast(0)).announce(anyString(), anyVararg());
-		verify(runner).runCommand(contains("debuild"));
-		verifyNoMoreInteractions(runner);
+		return descriptorImpl;
 	}
 }
 
